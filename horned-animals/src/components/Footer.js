1b14e0c1eca6201971db0d2192f44718289e0fa1
afnanshihab <afnan.shihab@yahoo.com>
2021-08-01 21:49:57 +0300

    solve the lab

---
+++ horned-animals/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <p>all copyrights reserved to &copy; Afnan Shihab 2021</p>
+            </div>
+        )
+    }
+}
+
+export default Footer
